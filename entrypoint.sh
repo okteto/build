48380cbf081aec99963d57e80975385ec792bd34
Ramiro Berrelleza <ramiro@okteto.com>
2023-07-24 15:36:05 -0700

    updated parameters

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 set -e
 
+tag=$1
 file=$2
+path=$3
+buildargs=$4
 global=$5
-
-BUILDPARAMS=""
 
 if [ ! -z "$OKTETO_CA_CERT" ]; then
    echo "Custom certificate is provided"
@@ -12,13 +13,24 @@ if [ ! -z "$OKTETO_CA_CERT" ]; then
    update-ca-certificates
 fi
 
-params=$(eval echo --progress plain -f "$file")
+params=""
 
-if [ "$global" = "true" ]; then
-    params="${params} --global"
+if [ ! -z $tag ]; then
+params="${params} --tag=${tag}"
 fi
 
-params=$(eval echo "$params")
+if [ ! -z $file ]; then
+params="${params} --file=${file}"
+fi
+
+if [ ! -z $global ]; then
+params="${params} --global"
+fi
+
+if [ ! -z $buildargs ]; then
+params="${params} --buildargs=${buildargs}"
+fi
+
 
 echo running: okteto build $params
 okteto build $params
