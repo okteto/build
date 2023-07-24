8d1162572f7224eb0f28d412d2a86073f58693d5
Ramiro Berrelleza <ramiro@okteto.com>
2023-07-24 15:52:52 -0700

    update parameters and readme

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,7 +7,7 @@ path=$3
 buildargs=$4
 global=$5
 
-if [ ! -z "$OKTETO_CA_CERT" ]; then
+if [ -n "$OKTETO_CA_CERT" ]; then
    echo "Custom certificate is provided"
    echo "$OKTETO_CA_CERT" > /usr/local/share/ca-certificates/okteto_ca_cert.crt
    update-ca-certificates
@@ -15,22 +15,27 @@ fi
 
 params=""
 
-if [ ! -z $tag ]; then
+if [ -n "$tag" ]; then
 params="${params} --tag=${tag}"
 fi
 
-if [ ! -z $file ]; then
+if [ -n "$file" ]; then
 params="${params} --file=${file}"
 fi
 
-if [ ! -z $global ]; then
+if [ "$global" = "true" ]; then
 params="${params} --global"
 fi
 
-if [ ! -z $buildargs ]; then
-params="${params} --buildargs=${buildargs}"
+if [ -n "$buildargs" ]; then
+IFS=',' ;for i in $buildargs; do 
+    params="${params} --build-arg=${i}"
+done
 fi
 
+if [ -n "$path" ]; then
+cd "$path"
+fi
 
-echo running: okteto build $params
-okteto build $params
+echo running: "okteto build $params"
+#okteto build $params
